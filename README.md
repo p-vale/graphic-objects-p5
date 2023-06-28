10f0d0eedea70367933a215067cd5dc733a67186
Valentina Pezzi <valente.pezzi@gmail.com>
2023-06-28 21:34:33 +0200

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # graphic-objects-p5
+
+Js creative coding examples using p5.js
+
+This project contains a few examples of generative art projects to test the library.
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Graphic Objects with p5.js',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
